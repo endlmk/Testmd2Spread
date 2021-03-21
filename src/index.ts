081d64bb1c8dc9ddc37e44732a6b71be85c4705b
endlmk <1269467+endlmk@users.noreply.github.com>
2021-03-21 22:36:35 +0900

    Extract text from headings.

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,17 @@ const contents = readFileSync('test.md', 'utf8')
 
 const result = md.parse(contents, {}) // Type error occcured without 2nd argument.
 console.log(result)
+
+let testTitle = ''
+const testSections: string[] = []
+for (let index = 0; index < result.length; index++) {
+  const element = result[index]
+  if (element.type === 'heading_open' && element.tag === 'h1') {
+    testTitle = result[index + 1].content
+  }
+  if (element.type === 'heading_open' && element.tag === 'h2') {
+    testSections.push(result[index + 1].content)
+  }
+}
+
+console.log(testTitle)
