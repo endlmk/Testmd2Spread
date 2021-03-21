83c3aef597937b13db0f413150e0c5f31947141d
endlmk <1269467+endlmk@users.noreply.github.com>
2021-03-21 20:50:20 +0900

    Fix prettier config according to JS Standard.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import MarkdownIt from "markdown-it/lib";
+import MarkdownIt from 'markdown-it/lib'
 // @ts-expect-error
-import markdownItTaskLists from "markdown-it-task-lists";
-import { readFileSync } from "fs";
+import markdownItTaskLists from 'markdown-it-task-lists'
+import { readFileSync } from 'fs'
 
-const md = MarkdownIt().use(markdownItTaskLists);
-const contents = readFileSync("test.md", "utf8");
+const md = MarkdownIt().use(markdownItTaskLists)
+const contents = readFileSync('test.md', 'utf8')
 
-const result = md.parse(contents, {}); // Type error occcured without 2nd argument.
-console.log(result);
+const result = md.parse(contents, {}) // Type error occcured without 2nd argument.
+console.log(result)
